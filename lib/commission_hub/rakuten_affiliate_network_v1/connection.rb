c306087ceb16bda1733741891fcafc398c99d61d
Felipe JAPM <felipe.japm@gmail.com>
2017-05-03 16:46:45 -0300

    add advanced reports endpoint

--- lib/commission_hub/rakuten_affiliate_network_v1/connection.rb
+++ lib/commission_hub/rakuten_affiliate_network_v1/connection.rb
@@ -2,10 +2,11 @@ module CommissionHub
   module RakutenAffiliateNetworkV1
     class Connection < CommissionHub::Connection
 
-      def_endpoint :authorization,   "token"
-      def_endpoint :events,          "events/1.0/transactions", class: :Base
-      def_endpoint :advertisers,     "advertisersearch/1.0",    class: :Base
-      def_endpoint :links,           "linklocator/1.0",         class: :Base
+      def_endpoint :authorization,    "token"
+      def_endpoint :events,           "events/1.0/transactions", class: :Base
+      def_endpoint :advertisers,      "advertisersearch/1.0",    class: :Base
+      def_endpoint :links,            "linklocator/1.0",         class: :Base
+      def_endpoint :advanced_reports, "advanced_reports/1.0",    class: :Base
       
       def initialize(settings)
         @settings = settings
