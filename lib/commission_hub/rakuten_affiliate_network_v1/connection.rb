64e5131332b4b9a70dca49e1441a995dfb3bf8fa
Felipe Jordão <felipe.japm@gmail.com>
2017-05-25 19:31:49 -0300

    Give simliar endpoint names to the api

--- lib/commission_hub/rakuten_affiliate_network_v1/connection.rb
+++ lib/commission_hub/rakuten_affiliate_network_v1/connection.rb
@@ -4,8 +4,8 @@ module CommissionHub
 
       def_endpoint :authorization,    "token"
       def_endpoint :events,           "events/1.0/transactions", class: :Base
-      def_endpoint :advertisers,      "advertisersearch/1.0",    class: :Base
-      def_endpoint :links,            "linklocator/1.0",         class: :Base
+      def_endpoint :advertisersearch,      "advertisersearch/1.0",    class: :Base
+      def_endpoint :linklocator,            "linklocator/1.0",         class: :Base
       def_endpoint :advanced_reports, "advanced_reports/1.0",    class: :Base
       
       def initialize(settings)
