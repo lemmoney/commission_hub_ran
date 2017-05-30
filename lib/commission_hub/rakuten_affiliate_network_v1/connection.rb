e923edcbb417ef5953f3ab14580da4782cd2ea0f
Felipe Jordão <felipe.japm@gmail.com>
2017-05-30 19:24:13 -0300

    Add coupon endpoint

--- lib/commission_hub/rakuten_affiliate_network_v1/connection.rb
+++ lib/commission_hub/rakuten_affiliate_network_v1/connection.rb
@@ -4,6 +4,7 @@ module CommissionHub
 
       def_endpoint :authorization,    "token"
       def_endpoint :events,           "events/1.0/transactions", class: :Base
+      def_endpoint :coupon,           "coupon/1.0", class: :Base
       def_endpoint :advertisersearch,      "advertisersearch/1.0",    class: :Base
       def_endpoint :linklocator,            "linklocator/1.0",         class: :Base
       def_endpoint :advanced_reports, "advanced_reports/1.0",    class: :Base
